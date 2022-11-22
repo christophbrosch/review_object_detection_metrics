832b63ff8ee36624612034b3ebfe52cea662631a
Christoph Brosch <christoph.brosch@outlook.de>
2022-11-22 20:24:27 +0100

    ready for the first test run

--- tests/test_package_api.py
+++ tests/test_package_api.py
@@ -10,6 +10,16 @@ def test_case_1():
 
     gts = converter.text2bb(gts_dir, BBType.GROUND_TRUTH)
     dets = converter.text2bb(dets_dir, BBType.DETECTED)
-    get_pascalvoc_metrics(gts, dets)
-    print(get_coco_summary(gts, dets))
-    
+    results_pascal_voc = get_pascalvoc_metrics(gts, dets)
+    results_coco = get_coco_summary(gts, dets)
+
+def test_case_2():
+    gts_dir = 'tests/test_coco_eval/gts'
+    dets_dir = 'tests/test_coco_eval/dets'
+
+    gts = converter.coco2bb(gts_dir, BBType.GROUND_TRUTH)
+    dets = converter.coco2bb(dets_dir, BBType.DETECTED)
+    results_pascal_voc = get_pascalvoc_metrics(gts, dets)
+    results_coco = get_coco_summary(gts, dets)
+
+    print(results_coco)
